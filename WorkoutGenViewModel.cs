23eedf4ab80eb0cdfa704252c9c1496ea9acd532
gujiaying1 <3508993191@qq.com>
2025-11-29 19:32:32 +0800

    Update WorkoutGenViewModel.cs

--- WorkoutGenViewModel.cs
+++ WorkoutGenViewModel.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections.Generic;
-using JoyRiseFitness.Models;
+using System.Collections.Generic;
 
 namespace JoyRiseFitness.Models
 {
+    public enum Goal { LoseFat, BuildMuscle, Strength, Endurance }
+    public enum Level { Beginner, Intermediate, Advanced }
+    public enum Gender { Male, Female, Other }
+
     public class WorkoutGenViewModel
     {
+        public Gender? Gender { get; set; }
+        public int? Age { get; set; } = 25;
+        public Goal? Goal { get; set; }
+        public Level? Level { get; set; }
         public MusclePart? Part { get; set; }
-        public string Equipment { get; set; }
+
         public List<Workout> Generated { get; set; } = new List<Workout>();
     }
 }
